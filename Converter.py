7a3decf5ad57cace25b9fa0f5da771fec221c0cc
Kotten <vide.zaar@gmail.com>
2025-07-12 13:16:20 +0200

    Add files via upload

---
+++ Converter.py
@@ -0,0 +1,29 @@
+from PIL import Image
+
+def rgb2hex(r,g,b):
+    return "#{:02x}{:02x}{:02x}".format(r,g,b)
+
+size = 32, 15
+
+image = Image.open("loka.jpg")
+
+image = image.resize(size)
+pixel_values = list(image.getdata())
+print(pixel_values)
+
+loreLine = ""
+
+for i in range(0, len(pixel_values)):
+    value = pixel_values[i]
+    loreLine += "<"+rgb2hex(value[0], value[1], value[2])+">█"
+    if((i+1)%32 == 0 and i != len(pixel_values)):
+        loreLine += "\n"
+
+loreLine = str(loreLine).replace(" ", '')
+print(loreLine)
+
+with open("Output.txt", "w", encoding="utf-8") as text_file:
+    text_file.write(loreLine)
+
+# ████████████████████████████████ 32x14
+
